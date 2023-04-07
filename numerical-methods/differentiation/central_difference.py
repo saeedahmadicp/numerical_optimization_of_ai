ac956ba381bdac39cfac5a7fbff0e776895ff1e2
Izhar Ali <aliizh94@students.rowan.edu>
2023-04-07 00:36:15 -0400

    differentiation added

--- numerical-methods/differentiation/central_difference.py
+++ numerical-methods/differentiation/central_difference.py
@@ -1,16 +1,16 @@
-def backward_difference(f, x, h):
+def central_difference(f, x, h):
     """
-    Backward difference method for numerical differentiation.
+    Central difference method for numerical differentiation.
     :param f: function
     :param x: point
     :param h: step size
     :return: derivative
     """
-    return (f(x) - f(x - h)) / h
+    return (f(x+h) - f(x-h)) / (2*h)
 
 
 if __name__ == "__main__":
     f = lambda x: x**2
     x = 2
     h = 0.01
-    print(backward_difference(f, x, h))
+    print(central_difference(f, x, h))
