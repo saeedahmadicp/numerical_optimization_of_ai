68ac1308ebd7c650ee539dd4e5eff1d522b7df38
Saeed Ahmad <saeedahmad.icp@gmail.com>
2023-05-10 13:36:40 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,4 @@
 from .initial_guess import findInitialInterval
 from .plot_roots import plotRootFindingMethods
-from .plot_lagrange import lagrange_interpolation_plot
-from .plot_spline import cubic_spline_plot
 
-__all__ = ["findInitialInterval", "plotRootFindingMethods", "lagrange_interpolation_plot", "cubic_spline_plot"]
+__all__ = ["findInitialInterval", "plotRootFindingMethods"]
