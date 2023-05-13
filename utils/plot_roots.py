b09c26559ee8525729335bf10c7c6cc22bc5930c
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-05-13 17:32:55 +0900

    visualization of elimination method added and fixed ploting method

--- utils/plot_roots.py
+++ utils/plot_roots.py
@@ -16,21 +16,17 @@ def plotRootFindingMethods(index, data):
         data[key]['iterations'] = [i for i in range(1, len(data[key]['errors'])+1)]
          
 
-    Figure = plt.figure(figsize=(5, 5))
-    Figure.set_facecolor("white")
-    Figure.set_edgecolor("black")
-    Figure.set_linewidth(2)
-    Figure.set_frameon(True)
-    Figure.set_dpi(100)
+    Figure = plt.figure()
+    
     
     ## plot the erros vs iterations for each method
     for key in data.keys():
-        plt.plot(data[key]['iterations'], data[key]['errors'], color=data[key]['color'], label=key)
+        plt.plot(data[key]['iterations'], data[key]['errors'], label=key)
     
-    plt.xlabel("Iterations", fontsize=15)
-    plt.ylabel("Absolute Errors", fontsize=15)
-    plt.title("Absolute Errors vs Iterations", fontsize=20)
-    plt.legend(title="Root finding methods", loc="upper right", fontsize=20)
+    plt.xlabel("Iterations")
+    plt.ylabel("Absolute Errors")
+    plt.title("Absolute Errors vs Iterations", )
+    plt.legend(title="Root finding methods", loc="upper right",)
     
     
     #plt.savefig(f'root_finding_methods_{index}.png')
