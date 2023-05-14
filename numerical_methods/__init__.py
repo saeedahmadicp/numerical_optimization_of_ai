cdd3ad4d74422d5dc33cb8066a038791973eb4d4
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-05-14 14:04:44 +0900

    resolved the import issues

--- numerical_methods/__init__.py
+++ numerical_methods/__init__.py
@@ -4,7 +4,9 @@ from .interpolation import lagrange_interpolation, cubic_spline
 from .lin_algebra import gauss_seidel, jacobi, gauss_elim, gauss_elim_pivot
 from .regression import linear, chebyshev
 from .root_finding import bisection, newton, regularFalsiMethod, secant, fibonacciSearchMethod, goldenSearchMethod, eliminationSearchMethod, nelderAndMeadMethod
+from .root_finding import powell_conjugate_direction_method, newton_hessian_method, steepest_descent_method
 
 _all__ = ['forward_difference', 'backward_difference', 'central_difference', 'trapezoidal', 'simpson', 'lagrange_interpolation', 'cubic_spline',
            'gauss_seidel', 'jacobi', 'gauss_elim', 'gauss_elim_pivot', 'linear', 'chebyshev', 'bisection', 'newton',
-             'regularFalsiMethod', 'secant', 'fibonacciSearchMethod', 'goldenSearchMethod', 'eliminationSearchMethod', 'nelderAndMeadMethod']
+             'regularFalsiMethod', 'secant', 'fibonacciSearchMethod', 'goldenSearchMethod', 'eliminationSearchMethod', 'nelderAndMeadMethod',
+             'newton_hessian_method', 'steepest_descent_method', 'powell_conjugate_direction_method']
